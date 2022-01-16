f8b4898978ab5d70888566b3e13a99412d7f038d
oliveryan <u6j3b@ugrad.cs.ubc.ca>
2022-01-15 22:10:25 -0800

    designed homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,24 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <header class="container">
-          <div class="row">
-            <h1 class="col-sm-1">
-              Hello
-            </h1>
-            <nav class="col-sm-11 text-right">
-              <p>Glucose</p>
-              <p>Contact</p>
-            </nav>
-          </div>
-        </header>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="Title"> Glucose Tracker</h1>
+        <p className="Description">Our Glucose Tracker helps record your glucose levels and an estimate of glucose intake 
+        throughout the day. By carefully monitoring glucose intake, health suggestions will be given.</p>
+        <div className="Gallery">
+          <img src={require('./images/Food.jpeg')} className="App-Food" height={500} width={900}/>
+          <img src={require('./images/Glucose.jpeg')} className="App-Glucose" height={500} width={900}/>
+        </div>
+        <div className="Links">
+          <p className="Foodlog">Click to log food</p>
+          <p className="Glucoselog">Click to log glucose</p>
+        </div>
       </header>
     </div>
   );

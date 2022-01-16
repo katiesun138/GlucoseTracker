012d66fa9dce4bb6b195ba29d13c7095e24f4f83
oliveryan <u6j3b@ugrad.cs.ubc.ca>
2022-01-15 20:21:58 -0800

    designing

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header class="container">
+          <div class="row">
+            <h1 class="col-sm-1">
+              Hello
+            </h1>
+            <nav class="col-sm-11 text-right">
+              <p>Glucose</p>
+              <p>Contact</p>
+            </nav>
+          </div>
+        </header>
         <a
           className="App-link"
           href="https://reactjs.org"

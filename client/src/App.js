412459da0e33a3bc0392a45fdf0dcf1544d131b3
katie <katiesun138@gmail.com>
2022-01-16 01:43:12 -0800

    adding hardcoded tables for food log

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -14,8 +14,38 @@ import { StartInput } from './components/StartInput';
 
 import {BrowserRouter as Router, Route, Routes, Link, Switch} from "react-router-dom";
 
+
 function App() {
+  // const [addFormData, setAddFormData] = useState({
+  //   foodName:"",
+  //   weightInG:"",
+  // });
+
+  // const handleAddFormChange = (event) => {
+  //   event.preventDefault();
+
+  //   const fieldName = event.target.getAttribute('foodItem');
+  //   const fieldValue = event.target.value;
+
+  //   const newFormData = {...addFormData};
+  //   newFormData[fieldName] = fieldValue;
+
+  //   setAddFormData(newFormData);
+  // };
+
+  // const handleAddFormSubmit = (event) => {
+  //   event.preventDefault();
+
+  //   const newContact ={
+  //     foodName: addFormData.foodName,
+  //     weightInG: addFormData.weightInG,
+  //   };
+  //   const newContact = [...contacts, newContact];
+  //   setContacts(newContact);
+  // };
+
   return (
+
     
     <Router>
       <nav>
@@ -42,7 +72,57 @@ function App() {
 }
 
 let Foodlog =()=>{
-  return( <h2>LOGGING FOOD HERE</h2>);
+  return( 
+    <div className='app-container'>
+      <h2>Welcome to Food Logging</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Food Item</th>
+            <th>Weight (g)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Apple</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>Omelette</td>
+            <td>300</td>
+          </tr>
+          <tr>
+            <td>Kale Salad</td>
+            <td>300</td>
+          </tr>
+          <tr>
+            <td>Steak and Sweet Potato</td>
+            <td>550</td>
+          </tr>
+        </tbody>
+      </table>
+      <h2>Add a Food Item</h2>
+      <form >
+        <input 
+        type="text" 
+        name="foodItem"
+        required="required"
+        placeholder='Enter a food item...'>
+        {/* onChange={handleAddFormChange} */}
+        </input>
+        <input 
+        type="text" 
+        name="itemWeight"
+        required="required"
+        placeholder='Enter weight in grams...'>
+        {/* onChange={handleAddFormChange} */}
+        </input>
+        <button type="submit">Add</button>
+      </form>
+      <h3>Your current Blood Sugar Prediction is <strong>135 mg/dL</strong></h3>
+    </div>
+      
+    );
 };
 let Home = () =>{
   return(
